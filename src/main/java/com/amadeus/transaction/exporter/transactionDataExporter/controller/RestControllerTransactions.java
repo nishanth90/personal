e45d8ca2bd32e43834d3e21db394ea49fa23a1b2
Nishanth KRISHNAMURTHY <nishanth.krishnamurthy@amadeus.com>
2019-10-04 12:39:16 +0530

    test

--- src/main/java/com/amadeus/transaction/exporter/transactionDataExporter/controller/RestControllerTransactions.java
+++ src/main/java/com/amadeus/transaction/exporter/transactionDataExporter/controller/RestControllerTransactions.java
@@ -1,14 +1,15 @@
 package com.amadeus.transaction.exporter.transactionDataExporter.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerTransactions {
 	
-	@RequestMapping("/transactions")
-	public void getTransactions() {
-		System.out.println("Hello Transactions!!!");
+	@RequestMapping(value = "/transactions", method= RequestMethod.GET)
+	public String getTransactions() {
+		return "Hello from Google Cloud Kubernetes!!!";
 	}
 
 }
